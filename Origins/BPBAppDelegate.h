b8f6805eee8ca421b5304b4e5d5beb8e7f70366a
billy <billy@spartansystems.co>
2014-04-23 15:53:46 -0400

    Initial Commit

---
+++ Origins/BPBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BPBAppDelegate.h
+//  Origins
+//
+//  Created by billy bray on 4/23/14.
+//  Copyright (c) 2014 Spartan Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BPBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OriginsTests/OriginsTests.m
@@ -0,0 +1,34 @@
+//
+//  OriginsTests.m
+//  OriginsTests
+//
+//  Created by billy bray on 4/23/14.
+//  Copyright (c) 2014 Spartan Systems. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OriginsTests : XCTestCase
+
+@end
+
+@implementation OriginsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
