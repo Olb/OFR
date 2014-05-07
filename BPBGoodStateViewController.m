4eb07341fc4b18570fd85d8966ed18a73459b30e
billy <billy@spartansystems.co>
2014-05-07 16:46:47 -0400

    Added transition for Good product tagging

--- BPBGoodStateViewController.m
+++ BPBGoodStateViewController.m
@@ -14,6 +14,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *productNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *storeNameLabel;
 @property (weak, nonatomic) IBOutlet UITextView *productDescriptionTextView;
+@property (weak, nonatomic) IBOutlet UIView *originSlideTagView;
+@property (weak, nonatomic) IBOutlet UIView *originSlideReplaceView;
 
 @end
 
@@ -47,12 +49,34 @@
 
 - (IBAction)tagItem:(id)sender
 {
-    [self.mvc addStore:self.storeName withImpact:self.impact andProduct:self.productBarcode andLocation:self.storeLocation];
-    // Add the product
-    [self.mvc addProduct:self.productName withImpact:self.impact andProductBarCode:self.productBarcode andImage:self.productImage withDescription:self.productDescription];
-    // Return to main view controller
-    [self.navigationController popToViewController:self.mvc animated:YES];
+//    [self.mvc addStore:self.storeName withImpact:self.impact andProduct:self.productBarcode andLocation:self.storeLocation];
+//    // Add the product
+//    [self.mvc addProduct:self.productName withImpact:self.impact andProductBarCode:self.productBarcode andImage:self.productImage withDescription:self.productDescription];
+//    // Return to main view controller
+//    [self.navigationController popToViewController:self.mvc animated:YES];
+    
+    [self replaceFirstTagView];
 }
 
+-(void)replaceFirstTagView
+{
+    // Remove the first view with tagging
+    [UIView transitionWithView:self.view
+                      duration:0.75
+                       options:UIViewAnimationOptionTransitionCrossDissolve
+                    animations:^{
+                        [self.originSlideTagView removeFromSuperview];
+                    }
+                    completion:nil];
+    
+    // Add new view for thank you
+    [UIView transitionWithView:self.view
+                      duration:0.75
+                       options:UIViewAnimationOptionTransitionCrossDissolve
+                    animations:^{
+                        [self.originSlideReplaceView setHidden:NO];
+                    }
+                    completion:nil];
+}
 
 @end
